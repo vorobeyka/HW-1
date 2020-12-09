56658016009e8dea12eb1650fa7f496868aca95f
vorobeyka <vorobeyka3222@gmail.com>
2020-12-10 01:11:15 +0200

    Done task-1 in level-2

--- level-2/task-1/Program.cs
+++ level-2/task-1/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace task_1
 {
     class Program
     {
+        /*
+        //I don't wanna do much much 'if' 'else' or 'switch' conditions for the game result,
+        //that's why I created this matrix with all game events.
+        //Please, enjoy my program.:)
+        */
+        static private int[][] gameMatrix = new int[3][];
+
         static private Commands userCommand;
         
         static private Commands computerCommand;
 
-        private static string[] commands = { "paper", "stone", "scissors" };
+        static private string[] commands = { "paper", "stone", "scissors" };
+
+        static private List<string> statistics = new List<string>(100);
 
         private enum Commands
         {
@@ -19,9 +29,10 @@ namespace task_1
             error
         }
 
-        static void ErrorHandle()
+        static int ErrorHandle()
         {
-            Console.WriteLine("Invalid command. Available commands: 'paper', 'stone', 'scissors', 'exit'.");
+            Console.WriteLine("Invalid command.\nAvailable commands: 'paper', 'stone', 'scissors', 'exit'.");
+            return 1;
         }
 
         static string RandomCommand()
@@ -42,50 +53,61 @@ namespace task_1
             }
         }
 
-        static void DoExit()
+        static int DoExit()
         {
+            Console.WriteLine("_________________________");
+            Console.WriteLine("_________________________");
+            Console.WriteLine("Results:");
 
+            for (int i = 0; i < statistics.Count; i++)
+            {
+                Console.WriteLine($"Round {i + 1}\n{statistics[i]}");
+                Console.WriteLine("_________________________");
+            }
+            Environment.Exit(0);
+            return 0;
         }
 
-        static void DoRound()
+        static int PrintRound()
         {
-            if (userCommand == computerCommand)
+            int i = (int) userCommand;
+            int j = (int) computerCommand;
+            string info = $"Player -> {commands[i]}\nComputer -> {commands[j]}\n";
+
+            Console.WriteLine($"Computer -> {commands[(int) computerCommand]}");
+            switch (gameMatrix[i][j])
             {
-                Console.WriteLine("NON");
-                return;
-            }
-            switch (userCommand)
-            {
-                case Commands.paper:
-                    if (computerCommand == Commands.scissors) Console.WriteLine("compuhter win");
-                    else Console.WriteLine("4elovek win");
+                case 1:
+                    Console.WriteLine("You won");
+                    statistics.Add(info + "Player WON!");
                 break;
-                case Commands.stone:
-                    if (computerCommand == Commands.paper) Console.WriteLine("compuhter win");
-                    else Console.WriteLine("4elovek win");
-                break;
-                case Commands.scissors:
-                    if (computerCommand == Commands.stone) Console.WriteLine("compuhter win");
-                    else Console.WriteLine("4elovek win");
-                break;
-                case Commands.exit:
-                    DoExit();
+                case -1:
+                    Console.WriteLine("You lose");
+                    statistics.Add(info + "Computer WON!");
                 break;
                 default:
-                    ErrorHandle();
+                    Console.WriteLine("Draw");
+                    statistics.Add(info + "DRAW!");
                 break;
+            }
+            return 0;
+        }
+
+        static int DoRound()
+        {
+            switch (userCommand)
+            {
+                case Commands.error: return ErrorHandle();
+                case Commands.exit: return DoExit();
+                default: return PrintRound();
             }
         }
 
-        static void Game()
+        static void InitGameMatrix()
         {
-            while (true)
-            {
-                Console.Write("Enter command -> ");
-                userCommand = SetCommand(Console.ReadLine());
-                computerCommand = SetCommand(RandomCommand());
-                DoRound();
-            }
+            gameMatrix[0] = new int[3] { 0, 1, -1};
+            gameMatrix[1] = new int[3] { -1, 0, 1};
+            gameMatrix[2] = new int[3] { 1, -1, 0};
         }
 
         static void Main(string[] args)
@@ -96,7 +118,15 @@ namespace task_1
             Console.WriteLine("The computer offer its version at random.");
             Console.WriteLine("stone > scissors; scissors > paper; paper > stone.");
             Console.WriteLine("To exit from application enter 'exit'.");
-            Game();
+            InitGameMatrix();
+            while (true)
+            {
+                Console.Write("Enter command -> ");
+                userCommand = SetCommand(Console.ReadLine());
+                computerCommand = SetCommand(RandomCommand());
+                DoRound();
+                Console.WriteLine("_________________________");
+            }
         }
     }
 }
