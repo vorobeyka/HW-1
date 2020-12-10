a6e7365f47d1e09885a3e3d5c6c03aaee71c1362
vorobeyka <vorobeyka3222@gmail.com>
2020-12-11 01:07:42 +0200

    Add task 2.3

--- level-2/task-2/Program.cs
+++ level-2/task-2/Program.cs
@@ -6,14 +6,15 @@ namespace task_2
     {
         private static bool _isCommandMode = false;
 
-        // private static string _error;
+        private static string _error;
 
         private static double _area;
 
         static bool ParseValue(string parseString, out double result)
         {
-            result = 1;
+            result = 0;
             if (parseString.Length > Double.MaxValue.ToString().Length) return false;
+            if (parseString.Contains(' ')) return false;
             if (!double.TryParse(parseString, out result)) return false;
             if (result <= 0) return false;
             return true;
@@ -26,17 +27,30 @@ namespace task_2
             double side2 = 0;
             double side3 = 0;
 
-            if (!ParseValue(args[1], out side1)
-                || !ParseValue(args[2], out side2)
-                || !ParseValue(args[3], out side3))
+            if (!ParseValue(args[1], out side1))
             {
+                _error = "Invalid first side. Try again.";
+                return -1;
+            }
+            if (!ParseValue(args[2], out side2))
+            {
+                _error = "Invalid second side. Try again.";
+                return -1;
+            }
+            if (!ParseValue(args[3], out side3))
+            {
+                _error = "Invalid third side. Try again.";
                 return -1;
             }
 
             double p = (side1 + side2 + side3) / 2.0;
             _area = Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
 
-            if (_area == double.NaN) return -1;
+            if (_area == double.NaN)
+            {
+                _error = "Invalid result. Try again.";
+                return -1;
+            }
             if (_isCommandMode) Console.WriteLine(Math.Round(_area, 4));
             return 0;
         }
@@ -46,7 +60,11 @@ namespace task_2
             if (args.Length != 2) return -1;
             double radius;
 
-            if (!ParseValue(args[1], out radius)) return -1;
+            if (!ParseValue(args[1], out radius))
+            {
+                _error = "Invalid radius. Try again.";
+                return -1;
+            }
 
             _area = Math.PI * Math.Pow(radius, 2);
             if (_isCommandMode) Console.WriteLine(Math.Round(_area, 4));
@@ -58,7 +76,10 @@ namespace task_2
             if (args.Length != 2) return -1;
             double side;
 
-            if (!ParseValue(args[1], out side)) return -1;
+            if (!ParseValue(args[1], out side)) {
+                _error = "Invalid side. Try again.";
+                return -1;
+            }
 
             _area = side * side;
             if (_isCommandMode) Console.WriteLine(Math.Round(_area, 4));
@@ -72,7 +93,16 @@ namespace task_2
             double side1;
             double side2;
             
-            if (!ParseValue(args[1], out side1) || !ParseValue(args[2], out side2)) return -1;
+            if (!ParseValue(args[1], out side1))
+            {
+                _error = "Invalid first side. Try again.";
+                return -1;
+            }
+            if (!ParseValue(args[2], out side2))
+            {
+                _error = "Invalid second side. Try again.";
+                return -1;
+            }
 
             _area = side1 * side2;
             if (_isCommandMode) Console.WriteLine(Math.Round(_area, 4));
@@ -95,15 +125,74 @@ namespace task_2
             }
         }
 
+        static string[] ReadRectParams()
+        {
+            Console.Write("Enter first side -> ");
+            string side1 = Console.ReadLine();
+            Console.Write("Enter second side -> ");
+            string side2 = Console.ReadLine();
+            return new string[]{ null, side1, side2 };
+        }
+
+        static string[] ReadCircleParams()
+        {
+            Console.Write("Enter radius -> ");
+            string radius = Console.ReadLine();
+            return new string[]{ null, radius };
+        }
+
+        static string[] ReadTriangleParams()
+        {
+            Console.Write("Enter first side -> ");
+            string side1 = Console.ReadLine();
+            Console.Write("Enter second side -> ");
+            string side2 = Console.ReadLine();
+            Console.Write("Enter third side -> ");
+            string side3 = Console.ReadLine();
+            return new string[]{ null, side1, side2, side3 };
+        }
+
+        static string[] ReadSquareParam()
+        {
+            Console.Write("Enter side -> ");
+            string side = Console.ReadLine();
+            return new string[]{ null, side };
+        }
+
         static void DialogMode()
         {
             Console.WriteLine("Task 2.2.Shape area calculation by Andrey Basystyi.");
             Console.WriteLine("Dear user. This program can help you to calculate shape area.");
-            Console.WriteLine("Rules: you need to enter one of these command: 'rectangle', 'square', 'circle', 'triangle'.");
-            Console.WriteLine("Then you must enter their ")
+            Console.WriteLine("Available commands: 'rectangle', 'square', 'circle', 'triangle'. Without sensetive case.");
+            Console.WriteLine("Then you must enter their parameters (more than 0).");
+            Console.WriteLine("Example for triangle: 3 4 5");
+            Console.WriteLine("For exit from program, enter -> 'exit.'\n");
+
             while (true)
             {
-
+                Console.Write("Enter command -> ");
+                string command = Console.ReadLine();
+                switch (command.ToLower())
+                {
+                    case "rect":
+                    case "rectangle": CalcRect(ReadRectParams());
+                    break;
+                    case "circle": CalcCircle(ReadCircleParams());
+                    break;
+                    case "triangle": CalcTriangle(ReadTriangleParams());
+                    break;
+                    case "square": CalcSquare(ReadSquareParam());
+                    break;
+                    case "exit":
+                        Console.WriteLine("Byyyye!");
+                        Environment.Exit(0);
+                    break;
+                    default: _error = "Invalid command. Available commands: 'rectangle', 'square', 'circle', 'triangle'.";
+                    break;
+                }
+                if (_error != null) Console.WriteLine(_error);
+                else Console.WriteLine($"Area = { Math.Round(_area, 4) }");
+                _error = null;
             }
         }
         
---
+++ level-2/task-3/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        private static int[] _array;
+
+        private static int _count;
+
+        private static int _minValue;
+
+        private static int _maxValue;
+
+        private static long _sum = 0;
+
+        private static double _average = 0;
+
+        private static double _rootMeanSquare = 0; 
+
+        static bool ParseValue(string value, out int result)
+        {
+            result = 0;
+            if (value.Contains(' ')) return false;
+            if (value.Length > int.MaxValue.ToString().Length) return false;
+            if (!int.TryParse(value, out result)) return false;
+            return true;
+        }
+
+        static void SortArray()
+        {
+            for (int i = 1; i < _count; i++)
+            {
+                var j = 0;
+                var buf = _array[i];
+                for (j = i - 1; j >= 0; j--)
+                {
+                    if (_array[j] < buf) break;
+                    _array[j + 1] = _array[j];
+                }
+                _array[j + 1] = buf;
+            }
+        }
+
+        static void CalculateAll()
+        {
+            SortArray();
+            _minValue = _array[0];
+            _maxValue = _array[_count - 1];
+            _average = _sum / _count;
+            foreach (var i in _array)
+                _rootMeanSquare += Math.Pow(i - _average, 2);
+            
+            _rootMeanSquare = Math.Round(Math.Sqrt(_rootMeanSquare / _count), 4);
+        }
+
+        static void ResultOut(bool isCommandMode)
+        {
+            if (isCommandMode) {
+                Console.WriteLine($"{_minValue}\n{_maxValue}\n{_sum}\n{_average}\n{_rootMeanSquare}");
+            } else {
+                Console.WriteLine($"Minimum = {_minValue}");
+                Console.WriteLine($"Maximum = {_maxValue}");
+                Console.WriteLine($"Sum of numbers = {_sum}");
+                Console.WriteLine($"Average = {_average}");
+                Console.WriteLine($"Root mean square = {_rootMeanSquare}");
+                Console.Write("Sorted array: ");
+            }
+            for (int i = 0; i < _count; i++)
+            {
+                Console.Write(_array[i]);
+                if (i + 1 != _count) Console.Write(" ");
+                else Console.WriteLine();
+            }
+        }
+
+        static int CommandMode(string[] args)
+        {
+            _count = args.Length;
+            _array = new int[_count];
+            for (int i = 0; i < _count; i++)
+            {
+                if (!ParseValue(args[i], out _array[i])) return -1;
+                _sum += _array[i];
+            }
+            CalculateAll();
+            ResultOut(true);
+            return 0;
+        }
+
+        static int DialogMode()
+        {
+            Console.Write("Enter length of array -> ");
+            if (!ParseValue(Console.ReadLine(), out _count) || _count <= 0)
+            {
+                Console.WriteLine("Invalid number, try again.");
+                return DialogMode();
+            }
+            Console.WriteLine("Push your array:");
+
+            _array = new int[_count];
+            for (int i = 0; i < _count; )
+            {
+                Console.Write("-> ");
+                if (!ParseValue(Console.ReadLine(), out _array[i]))
+                {
+                    Console.WriteLine("Invalid number, try again.");
+                    continue;
+                }
+                _sum += _array[i];
+                i++;
+            }
+            CalculateAll();
+            ResultOut(false);
+            return 0;
+        }
+        
+        static int Main(string[] args)
+        {
+            if (args.Length > 0) return CommandMode(args);
+            Console.WriteLine("Task 2.3.Array statistics by Andrey Basystyi.");
+            Console.WriteLine("This program will help you to sort array and find:");
+            Console.WriteLine("-minimum and maximum values\n-sum of all elements\n-average\n-root mean square.\n");
+            return DialogMode();
+        }
+    }
+}
